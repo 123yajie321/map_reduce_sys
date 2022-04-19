00b59ab810e6d0b0f42b7a1787c4ac6d3071b12a
LIU Yajie <yajie.liu@u-psud.fr>
2022-04-19 11:15:56 +0200

    update

---
+++ src/map_reduce_sys/DistributedCVM.java
@@ -0,0 +1,16 @@
+package map_reduce_sys;
+import fr.sorbonne_u.components.cvm.AbstractDistributedCVM;
+
+public class DistributedCVM extends AbstractDistributedCVM{
+
+	public DistributedCVM(String [] args) throws Exception {
+		super(args);	
+	}
+	
+	@Override
+	public void instantiateAndPublish() {
+		
+		
+	}
+
+}
